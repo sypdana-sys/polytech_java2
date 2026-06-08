9edbae7367ccfc0181ac8de66cad763814abaaa5
Sungyeon.Park <sypdana@gmail.com>
2026-06-08 10:30:35 +0900

    Java 기본기 연습

---
+++ day01-basic/src/main/java/com/survivalcoding/MyfristClass.java
@@ -0,0 +1,11 @@
+package com.survivalcoding;
+
+public class MyfristClass {
+    static void main(String[] args) {
+        int i = 0;
+        i = 10;
+
+        // sout enter
+        System.out.println("hello");
+    }
+}
